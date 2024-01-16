de3a5cac66bd94dd536517a3a8a50043a26df412
meoree <cokoson@yandex.ru>
2024-01-16 17:27:13 +0300

    SSH connection via jump host using Paramiko

---
+++ paramiko_jump_host/ssh_jump.py
@@ -0,0 +1,36 @@
+import paramiko
+import time
+
+from useful.paramiko_jump_host.client import SSHJumpClient
+
+with paramiko.SSHClient() as jumper:
+    jumper.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    jumper.connect(
+        hostname='jump_host_ip_address',
+        username='jump_host_username',
+    )
+    # Now instantiate a session for the Jump Host <-> Target
+    # Host connection, and inject the jump_session to use for
+    # proxying.
+    target = SSHJumpClient(jump_session=jumper)
+    target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    target.connect(
+        hostname='target_host_ip_address',
+        username='targer_host_username',
+        password='target_host_password',
+        look_for_keys=False,
+        allow_agent=False,
+    )
+    with target.invoke_shell() as ssh:
+
+        # Example
+
+        time.sleep(1)
+        ssh.recv(20000)
+        ssh.send("\n")
+        ssh.send("ip a\n")
+        time.sleep(1)
+        output = ssh.recv(10000).decode("utf-8")
+        print(output)
+
+
